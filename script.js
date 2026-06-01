b6ce1eb7fcf2708a1bfb24757a82fb1df524b125
buenosilva29-code <bueno.silva29@escola.pr.gov.br>
2026-06-01 09:30:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script simples para interatividade do botão
+document.getElementById('join-btn').addEventListener('click', function() {
+    alert('Obrigado por se inscrever no Agrinho 2026! 🌻');
+});
